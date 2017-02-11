f50566303dadd968dcf71fc2884fdea3531ef9ae
MitchellHamm <mitchellphamm@hotmail.com>
2017-02-11 11:44:08 -0600

    Added base files along with init methods from class, these should be everything we need to get the bulk of the assignment working with the timmer interrupts and button interrupts

---
+++ Assignment 1/main.c
@@ -0,0 +1,114 @@
+#include "stm32f4xx.h"
+
+GPIO_InitTypeDef GPIO_Initstructure;
+TIM_TimeBaseInitTypeDef timer_InitStructure;
+EXTI_InitTypeDef EXTI_InitStructure;
+NVIC_InitTypeDef nvicStructure;
+NVIC_InitTypeDef NVIC_InitStructure;
+
+void InitLEDs()
+{
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+ 
+ 	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Initstructure.GPIO_OType = GPIO_OType_PP;  
+	GPIO_Initstructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_Init(GPIOD, &GPIO_Initstructure);
+}
+
+void InitTimers()
+{
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+      
+  timer_InitStructure.TIM_Prescaler = 83;
+  timer_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  timer_InitStructure.TIM_Period = 99999;
+  timer_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+  timer_InitStructure.TIM_RepetitionCounter = 0;
+  TIM_TimeBaseInit(TIM2, &timer_InitStructure);
+  TIM_Cmd(TIM2, ENABLE);
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+}
+
+void EnableTimerInterrupt()
+{
+    nvicStructure.NVIC_IRQChannel = TIM2_IRQn;
+    nvicStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    nvicStructure.NVIC_IRQChannelSubPriority = 1;
+    nvicStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&nvicStructure);
+}
+
+void InitButton() // initialize user button
+{
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); 
+	
+	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_Initstructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_Initstructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_Init(GPIOA, &GPIO_Initstructure);
+}
+
+void InitEXTI()
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+    // Selects the GPIOA pin 0 used as external interrupt source
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+    
+    EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_Init(&EXTI_InitStructure);
+}
+
+void EnableEXTIInterrupt()
+{
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+void TIM2_IRQHandler()
+{
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+    {
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+        GPIO_ToggleBits(GPIOD, GPIO_Pin_15);
+    }
+}
+
+void EXTI0_IRQHandler()
+{
+    // Checks whether the interrupt from EXTI0 or not
+    if (EXTI_GetITStatus(EXTI_Line0) != RESET)
+    {      
+	GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+	// Clears the EXTI line pending bit
+        EXTI_ClearITPendingBit(EXTI_Line0);			
+    }
+}
+
+int main() {
+	InitLEDs();
+	InitTimers();
+	EnableTimerInterrupt();
+	InitButton();
+	InitEXTI();
+	EnableEXTIInterrupt();
+	
+	while(1){
+		int i = 0;
+		for (i = 0; i < 1000000; i++)
+    1+1;
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
+	}
+
+}
